efcdb66abc824616e6915a7fcad906cd50033bb9
Arndt <jarndt@htwk-leipzig.de>
2017-11-30 12:57:33 +0100

    Jacobi generalised

--- Jacobi.m
+++ Jacobi.m
@@ -2,7 +2,7 @@
 #
 # Fuer das Numerik I Seminar.
 
-function res = Jacobi(A, b, eps=0.00001)
+function res = Jacobi(A, b, x0=zeros(size(b)), eps=0.00001, omega = 1, doTests=true)
   res = 0;
   # Pretest for the Jacobi Methode
   
@@ -22,33 +22,52 @@ function res = Jacobi(A, b, eps=0.00001)
     return;
   endif
   
-  # Row-Sum-Criterea
-  r = RowSumTest(A);
-  # Column-Sum Criterea
-  c = RowSumTest(transpose(A));
-  # Square-Sum-criterea
-  s = SquareSumTest(A);
-  
-  if!(r || c || s)
-  	if !r
-		printf("Row-Sum-Criterea Faild!\n")
-	elseif !c
-		printf("Column-Sum-Criterea Faild!\n")
-	elseif !s
-		printf("Square-Sum-Criterea Faild!\n")
-	endif
-  return
+  if doTests 
+    # Row-Sum-Criterea (Inf-Norm)
+    r = RowSumTest(A);
+    # Column-Sum Criterea (1-Norm)
+    c = RowSumTest(transpose(A));
+    # Square-Sum-criterea (Frobenius-Norm)
+    s = SquareSumTest(A);
+    
+    if!(r || c || s)
+      if !r
+      printf("Row-Sum-Criterea Faild!\n")
+    elseif !c
+      printf("Column-Sum-Criterea Faild!\n")
+    elseif !s
+      printf("Square-Sum-Criterea Faild!\n")
+    endif
+    return
+    endif
   endif
   
   # Prepare Jacobi Methode
   B = diag(diag(A));
-  M = inv(B)*(B-A)
-  Nb = inv(B)*b;
+  M = inv(B)*(B-omega*A)
+  Nb = omega*inv(B)*b;
   
-  #Calculate needed iteration depth.
-  q = max(abs(eig(M)))
-  
-  depth = log(eps*(1-q)/norm(b, inf))/log(q)
+  # Calculate iteration depth if eps < 1 else depth = eps
+  depth = 0;
+  if eps < 1
+    nor = 0;
+    #Chose Norm
+    if r
+      nor = inf;
+    elseif c
+      nor = 1;      
+    elseif s
+      nor = "fro";      
+    endif
+    
+    #TODO: x1 missing
+    
+    #Calculate needed iteration depth.
+    q = norm(A, nor);
+    depth = ceil (log(eps*(1-q)/norm(b, nor))/log(q));
+  else
+    depth = ceil (eps);
+  endif
   
   # Iterate through Jacobi
   res = iterate(zeros(size(b), 1), M, Nb, depth);
@@ -82,10 +101,12 @@ function res = RowSumTest(A)
   endif
   
   # Test for connectivity through algebraic connectivity.
-  addpath ("./octave-networks-toolbox/");
+  libNetwork = "./octave-networks-toolbox/";
+  addpath (libNetwork);
   
   res = algebraicConnectivity(A) > 0;
   
+  rmpath(libNetwork);
 endfunction
 
 function res = SquareSumTest(A)
